60ad7e718bd750ee1fd41589d03e2e43fe125576
Jonathan Pereira Dias <52224050+JhonDias98@users.noreply.github.com>
2021-06-04 02:00:08 -0300

    Lidando com erros retornados pela API gRPC

--- src/main/kotlin/br/com/zup/CalculadoraDeFretesController.kt
+++ src/main/kotlin/br/com/zup/CalculadoraDeFretesController.kt
@@ -1,10 +1,16 @@
 package br.com.zup.calculadora
 
 import br.com.zup.CalculaFreteRequest
+import br.com.zup.ErrorDetails
 import br.com.zup.FretesServiceGrpc
+import io.grpc.Status
+import io.grpc.StatusRuntimeException
+import io.grpc.protobuf.StatusProto
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.QueryValue
+import io.micronaut.http.exceptions.HttpStatusException
 import javax.inject.Inject
 
 @Controller
@@ -15,11 +21,29 @@ class CalculadoraDeFretesController(@Inject val grpcClient: FretesServiceGrpc.Fr
         val request = CalculaFreteRequest.newBuilder()
             .setCep(cep)
             .build()
-        val response = grpcClient.calculaFrete(request)
-        return FreteResponse(
-            cep = response.cep,
-            valor = response.valor
-        )
+
+        try {
+            val response = grpcClient.calculaFrete(request)
+            return FreteResponse(cep = response.cep, valor = response.valor)
+        } catch (e: StatusRuntimeException) {
+            val statusCode = e.status.code
+            val description = e.status.description
+
+            if(statusCode == Status.Code.INVALID_ARGUMENT) {
+                throw HttpStatusException(HttpStatus.BAD_REQUEST, description)
+            }
+
+            if(statusCode == Status.Code.PERMISSION_DENIED) {
+                val statusProto = StatusProto.fromThrowable(e) ?: throw
+                                    HttpStatusException(HttpStatus.FORBIDDEN, description)
+                val anyDetails = statusProto.detailsList[0]
+                val errorDetails = anyDetails.unpack(ErrorDetails::class.java)
+                throw HttpStatusException(HttpStatus.FORBIDDEN, "${errorDetails.code}: ${errorDetails.message}")
+            }
+
+            throw HttpStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.message)
+        }
+
     }
 }
 
